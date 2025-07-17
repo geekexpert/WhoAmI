9cd17b5e070e7370392bda4b3ed8e236ae28cd7a
John Silva <john.silva@datacom.com>
2025-07-17 22:38:00 +1200

    initial http client and controllers creation

---
+++ WhoAmIAPI/Models/PersonCreationModel.cs
@@ -0,0 +1,43 @@
+using System.Text.Json.Serialization;
+
+namespace WhoAmIAPI.Models
+{
+    public class AgifyResponse
+    {
+        public string Name { get; set; }
+        public int? Age { get; set; }
+    }
+
+    public class GenderizeResponse
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+
+    public class CountryProbability
+    {
+        [JsonPropertyName("country_id")]
+        public string CountryId { get; set; }
+        public double Probability { get; set; }
+    }
+
+    public class NationalizeResponse
+    {
+        public string Name { get; set; }
+        public List<CountryProbability> Country { get; set; }
+    }
+
+    public class CountryInfo
+    {
+        public string Country { get; set; } = "";
+        public double Probability { get; set; }
+    }
+
+    public class PersonInfo
+    {
+        public string Name { get; set; }
+        public int? Age { get; set; }
+        public string Gender { get; set; }
+        public List<CountryInfo> Nationality { get; set; } = new();
+    }
+}
--- WhoAmIAPI/Program.cs
+++ WhoAmIAPI/Program.cs
@@ -16,5 +16,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapControllers();
+
 app.Run();
 
